b10434d9f9d1a19e5c69be8ff5d1c084ad009ed5
Rarenayab520 <nayabasmat121@gmail.com>
2025-01-20 16:34:00 +0100

    .

--- app/src/main/java/com/nayab/myfirstapp/RecyclerviewActtivity.kt
+++ app/src/main/java/com/nayab/myfirstapp/RecyclerviewActtivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerviewActtivity : AppCompatActivity() {
+    private  lateinit var rvUser:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recyclerview_acttivity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        rvUser=findViewById(R.id.)
+        //1 Recycler view
+        //2 item design
+        //3  list->type(class) pass-> Adapter
+        //4 rv frontend layout
+        //5 list pass Adapter set
+        //6 Adapter class ->oncreateviewholder(viewholder)  onbindviewholder(data set ) item getcount
+
     }
 }
