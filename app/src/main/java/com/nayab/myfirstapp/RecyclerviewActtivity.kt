2595ef6bdfb490b776013ddd1acf08686dd774e8
Rarenayab520 <nayabasmat121@gmail.com>
2025-01-24 11:53:15 +0100

    .

--- app/src/main/java/com/nayab/myfirstapp/RecyclerviewActtivity.kt
+++ app/src/main/java/com/nayab/myfirstapp/RecyclerviewActtivity.kt
@@ -2,6 +2,7 @@ package com.nayab.myfirstapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,21 +10,43 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RecyclerviewActtivity : AppCompatActivity(), Adapter.itemclicklistner {
     private  lateinit var rvUser:RecyclerView
-
+    private  lateinit var btnGetdata:Button
+    private val database = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recyclerview_acttivity)
         rvUser=findViewById(R.id.rvUsers)
+        //Retrive data from firebase
+        btnGetdata=findViewById(R.id.btnGetdata)
         var listUser=ArrayList<ModelUser>()
-        listUser.add(ModelUser("Coach", "Noob eik kam karo..!"))
-        listUser.add(ModelUser("Hadiii", "kesi ho ?"))
-        listUser.add(ModelUser("Nayab", "kidr ho ?"))
-        listUser.add(ModelUser("Noob", "Alhamdulilah"))
-        listUser.add(ModelUser("Rare", "How are you?"))
+        btnGetdata.setOnClickListener()
+        {
+            database.collection("Username").get()
+                .addOnSuccessListener { documents->
+                        for (document in documents)
+                            {
+                              listUser.add(document.toObject(ModelUser::class.java))
+                            }
+                    rvUser.adapter=Adapter(listUser,this@RecyclerviewActtivity)
+                }
+                .addOnFailureListener {
+
+                }
+        }
+
+
+
+
+       // listUser.add(ModelUser("Coach", "Noob eik kam karo..!"))
+       // listUser.add(ModelUser("Hadiii", "kesi ho ?"))
+       // listUser.add(ModelUser("Nayab", "kidr ho ?"))
+       // listUser.add(ModelUser("Noob", "Alhamdulilah"))
+        //listUser.add(ModelUser("Rare", "How are you?"))
         rvUser.layoutManager=LinearLayoutManager(this@RecyclerviewActtivity)
         rvUser.adapter=Adapter(listUser,this@RecyclerviewActtivity)
 
@@ -37,6 +60,6 @@ class RecyclerviewActtivity : AppCompatActivity(), Adapter.itemclicklistner {
 
     override fun itemclick(modelUser: ModelUser) {
         //Toast.makeText(this@RecyclerviewActtivity,modelUser.username, Toast.LENGTH_SHORT).show()
-        startActivity(Intent(this@RecyclerviewActtivity,Activity_card_view::class.java).putExtra("Name",modelUser.email).putExtra("Password",modelUser.password))
+        startActivity(Intent(this@RecyclerviewActtivity,Activity_card_view::class.java))
     }
 }
